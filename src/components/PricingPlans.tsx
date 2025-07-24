3416e74ca806b4471daaa426b3ee6f130bf5c0dc
Nitish Kumar Bhambu <145218769+ThereIsSomething@users.noreply.github.com>
2025-07-25 00:43:24 +0530

    Updated PricingPlans.tsx

--- src/components/PricingPlans.tsx
+++ src/components/PricingPlans.tsx
@@ -218,9 +218,9 @@ const PricingPlans = () => {
   key={`${planType}-${plan.name}`}
   className={`relative group animate-slide-up-fade transform transition-all duration-500 hover:scale-105 ${
     plan.popular
-      ? 'ring-2 ring-purple-500/50 z-10'
+      ? 
       : plan.premium
-        ? 'ring-2 ring-yellow-400/50 z-10'
+        ? 
         : ''
   } ${plan.popular ? 'md:col-span-2 lg:col-span-1' : ''}`}
   style={{ animationDelay: `${index * 200}ms` }}
