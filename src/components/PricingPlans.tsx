6a6980dbdfdc75ba8d376ebea115a551df386b08
Nitish Kumar Bhambu <145218769+ThereIsSomething@users.noreply.github.com>
2025-07-24 17:44:35 +0530

    Updated PricingPlans.tsx

--- src/components/PricingPlans.tsx
+++ src/components/PricingPlans.tsx
@@ -225,7 +225,8 @@ const PricingPlans = () => {
               } ${plan.popular ? 'md:col-span-2 lg:col-span-1' : ''}`}
               style={{animationDelay: `${index * 200}ms`}}
             >
-              <div className={`aqua-glass-dark rounded-3xl p-6 sm:p-8 h-full lg:min-h-[720px] hover:scale-105 transition-all duration-500 relative overflow-hidden flex flex-col ${
+              <div className={`aqua-glass-dark rounded-3xl p-6 sm:p-8 h-full lg:min-h-[720px] hover:ring-4 hover:ring-purple-400/30
+ transition-all duration-500 relative overflow-hidden flex flex-col ${
               plan.popular ? 'ring-2 ring-purple-500/50 transform scale-105' : ''
               }`}>
 
